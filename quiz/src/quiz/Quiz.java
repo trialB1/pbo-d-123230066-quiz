dc0b6666c1f841b8dea3752259eae123a04151e4
Bayu Java <118042163+trialB1@users.noreply.github.com>
2025-03-11 12:35:10 +0700

    Delete quiz directory

--- quiz/src/quiz/Quiz.java
+++
@@ -1,9 +0,0 @@
-package quiz;
-
-public class Quiz {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            new HalamanLogin();
-        });
-    }
-}
